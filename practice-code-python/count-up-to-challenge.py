1fb7f2ea29e99450a33a6df1317a6414f6fb87de
Elvira Sidorenko <elvira.sidorenko@developersinstitute.co.nz>
2023-11-22 10:50:26 +1300

    Count up to challenge

---
+++ practice-code-python/count-up-to-challenge.py
@@ -0,0 +1,14 @@
+n = int(input('Type a number: '))
+list = ''
+
+# Prints 1 through to n in square brackets like [1][2][3]...[n]
+
+while n <= 0:
+    n = int(input('Not a valid number. Try again.'))
+
+i = 1
+while i <= n:
+    list = list + f'[{i}]'
+    i += 1
+
+print(list)
