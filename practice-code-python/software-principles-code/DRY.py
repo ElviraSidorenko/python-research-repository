007b2c3d9c5570539f8172eefd24d89c47b9257c
Elvira Sidorenko <elvira.sidorenko@developersinstitute.co.nz>
2023-11-27 11:08:16 +1300

    Update DRY principle comments

--- practice-code-python/software-principles-code/DRY.py
+++ practice-code-python/software-principles-code/DRY.py
@@ -1,17 +1,21 @@
+
+# DRY: Don't Repeat Yourself
 # Principle: Avoid duplicating code. Encourage code reuse to reduce redundancy.
 
 # Bad Example
 def add_numbers(a, b):
+    # Duplicated logic for addition
     print(a + b)
 
 
 def subtract_numbers(a, b):
+    # Duplicated logic for subtraction
     print(a - b)
 
+
 # Good Example
-
-
 def perform_operation(a, b, operation):
+    # Reusable function for performing various operations
     if operation == 'add':
         print(a + b)
     elif operation == 'subtract':
