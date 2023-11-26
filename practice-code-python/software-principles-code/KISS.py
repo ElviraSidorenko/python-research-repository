5de87a19dd3d153b85d95845249a9b9ecbd43446
Elvira Sidorenko <elvira.sidorenko@developersinstitute.co.nz>
2023-11-27 11:23:02 +1300

    Add KISS principle

---
+++ practice-code-python/software-principles-code/KISS.py
@@ -0,0 +1,20 @@
+# KISS: Keep it simple, stupid
+# Principle: Keep the design and implementation as simple as possible.
+
+# Bad Example
+def complicated_loop(data):
+    result = 0
+    for i in range(len(data)):
+        # Complex loop logic with unnecessary details
+        result += data[i] * (i + 1)
+    return result
+
+# Good Example
+
+
+def simple_loop(data):
+    result = 0
+    for value in data:
+        # Simple loop logic with clear iteration
+        result += value
+    return result
