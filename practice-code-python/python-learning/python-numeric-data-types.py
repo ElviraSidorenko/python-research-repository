11ca2cdede7f248475517fad45d150edd1864559
Elvira Sidorenko <elvira.sidorenko@developersinstitute.co.nz>
2023-11-27 13:49:52 +1300

    Learn Python numeric data types

---
+++ practice-code-python/python-learning/python-numeric-data-types.py
@@ -0,0 +1,13 @@
+# int (integer): Integers are whole numbers, both positive and negative. They do not have a decimal point. For example:
+
+x = 5
+y = -10
+
+# float (floating-point): Floating-point numbers, or floats, represent real numbers with a decimal point. They can also use scientific notation. For example:
+
+a = 3.14
+b = 2.0e-3  # 2.0 * 10^(-3)
+
+# complex: Complex numbers are used to represent numbers with both a real and an imaginary part. They are written with a "j" or "J" to denote the imaginary part. For example:
+
+z = 2 + 3j
