c1314a31e48f35e55f3bda649425df0f26574713
Elvira Sidorenko <elvira.sidorenko@developersinstitute.co.nz>
2023-11-27 13:45:13 +1300

    Learn Python Byte Object

---
+++ practice-code-python/python-learning/python-byte-object.py
@@ -0,0 +1,14 @@
+
+# Python Byte Object
+
+# Streaming files
+# Transmitting text without knowing the encoding
+# Often used 'under the hood' in Python libraries
+
+
+# Creates an empty bytes object that is 4-bytes long
+
+bytes(4)  # b'\x00\x00\x00\x00'
+
+
+# Bytes object has a 'b' in front of it to differentiate it from the string.
