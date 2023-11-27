616ad8a7baa81970ad402c92f4edcaae47ef7649
Elvira Sidorenko <elvira.sidorenko@developersinstitute.co.nz>
2023-11-27 16:23:58 +1300

    Add ISP

---
+++ practice-code-python/software-principles/interface-segregation-principle.py
@@ -0,0 +1,3 @@
+# ISP: Interface Segregation Principle
+# The Interface Segregation Principle states that clients should not be forced to rely on methods they do not use and therefore suggests the creation of specific interfaces or classes for such clients.
+
