f0bfff91e38a7832872c226dce9dcba1f8b77700
Elvira Sidorenko <elvira.sidorenko@developersinstitute.co.nz>
2023-11-30 09:58:35 +1300

    Add comments to the challenge

--- practice-code-python/python-challenges/sum-of-numbers-challenge.py
+++ practice-code-python/python-challenges/sum-of-numbers-challenge.py
@@ -1,4 +1,7 @@
-# Computes the sum of the first n positive integers like sum = 1 + 2 + 3 + ... + n
+# Challenge: Write a Python program that calculates the sum of all positive integers from 1 to a user-provided positive number (n).
+# The program starts by prompting the user to input a positive number and validates the input to ensure it is indeed a positive integer.
+# If the user provides a non-positive number, the program prompts the user to try again until a valid input is received.
+
 
 # Get the input
 n = int(input('Type a positive number: '))
@@ -9,6 +12,7 @@ sum = 0
 while n <= 0:
     n = int(input('Your number is not valid. Try again: '))
 
+# Compute the sum of the first n positive integers like sum = 1 + 2 + 3 + ... + n
 while i <= n:
     sum = sum + i
     i += 1
